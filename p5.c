72c101b45fd6b4c310163193cde6428e8de847d5
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2019-09-02 16:54:07 +0200

    Primtal

---
+++ p5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+
+  int tal = atoi(argv[1]);
+  
+  float roten = sqrt(tal);
+  int limit = floor(roten) + 1;
+
+  int x = 2;
+  int y = 2;
+  if (x == y)
+    {
+      puts("LIKA");
+    }
+  else
+    {
+      puts("OLIKA");
+    }
+
+
+
+}
+  
