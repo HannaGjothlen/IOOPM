627c1328b57552a9c6ceffb331484fdd47aabd0e
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2019-09-03 12:00:37 +0200

    Add files via upload

--- p5.c
+++ p5.c
@@ -2,26 +2,24 @@
 #include <stdlib.h>
 #include <math.h>
 
+ 
 int main(int argc, char *argv[])
 {
+  int n = atoi(argv[1]), c = 2;
+   
+   for ( c = 2 ; c <= n - 1 ; c++ )
+   {
+      if ( n%c == 0 )
+      {
+         printf("%d isn't prime.\n", n);
+     break;
+      }
+   }
+   if ( c == n )
+      printf("%d is prime.\n", n);
 
-  int tal = atoi(argv[1]);
-  
-  float roten = sqrt(tal);
-  int limit = floor(roten) + 1;
-
-  int x = 2;
-  int y = 2;
-  if (x == y)
-    {
-      puts("LIKA");
-    }
-  else
-    {
-      puts("OLIKA");
-    }
-
-
-
+   if (n == 1)
+     printf("%d is not prime.\n", n);
+       
+   return 0;
 }
-  
