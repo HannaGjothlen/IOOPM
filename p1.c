72252549aaa2186e9cb516c12f2207a76b740f7e
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2019-09-04 12:07:02 +0200

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i;              // deklaration och initiering av iterationsvariabeln
+  for (i = 1; i <= 10; i++)         // iterationsvillkor (utför blocket så länge i är mindre än 11)
+  {                       // loop-kropp (utförs så länge iterationsvillkoret är uppfyllt)
+    printf("%d\n", i);    // skriv ut 1, och en radbrytning
+                          // öka i:s värde med 1 (förändring av iterationsvariabeln)
+  }
+  return 0;
+}
