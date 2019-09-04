dafcadd3b0ae0ddc05a50256404e434ca47bd265
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2019-09-04 15:50:14 +0200

    Add files via upload

--- p6.c
+++ p6.c
@@ -1,38 +1,68 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+bool is_number(char *str)
+{
+  int n = strlen(str);
+
+  
+  for(int i=0; i<n; i++)
+    {
+      if (str[i]=='-'&& i==0 && n>1)
+	{
+	  continue;
+	}
+      if (isdigit(str[i]))
+	{
+       
+	}
+     
+      else
+	{
+	  return false;
+	}
+    }
+  return true;
+}
+
+
 
 int main(int argc, char *argv[])
 {
+		
   int n = atoi(argv[1]);
   int m = atoi(argv[2]);
-  
-  for (n, n==m, 
+  int a = n;
+  int b = m;
+  while (is_number(argv[1]) && is_number(argv[2])&& n>=0 && m>=0)
+    {
+      if (  n!=m)
+    {
+ 	 	
+      if (n>m)
+	{
+	  n = n - m;
+	  
+	}
 
-    (n == m)
-   {
-     printf("gcd(%d, %d) = %d\n", n, m, n);
-     n++;
+      else if  (n < m)
+	{
+	  m = m - n;
+        
+	}
+    
    }
- 
-
-  while (n > m)
-      {
-        n = n - m;
-        printf("gcd(%d, %d) = %d\n", n, m, n);
-      }
-
-     
-   while (n < m)
-      {
-	 m = m - n;
-	 printf("gcd(%d, %d) = %d\n", m, n, m);
-      }
-
-   return 0;
-
+  printf("gcd(%d, %d) = %d\n", a, b, n);
+  return 0;
+  }
+   printf("error\n");
+  return 0;   
 }
-   
+
   
       
   
