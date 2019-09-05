c57d758864ee66f336c9bea2d6f8d137bc0e2ac3
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2019-09-05 16:55:05 +0200

    Add files via upload

---
+++ utils.c
@@ -0,0 +1,71 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include "utils.h"
+
+int clear_input_buffer()
+{
+  int c;
+     do
+        {
+          c = getchar();
+        }
+      while (c != '\n' && c != EOF);
+      putchar('\n');
+      return 0;
+}
+
+int ask_question_int(char *question)
+{
+
+  int result = 0;
+  int conversions = 0;
+  do
+    {
+      printf("%s\n", question);
+      conversions = scanf("%d", &result);
+      clear_input_buffer();
+    }
+  while (conversions < 1);
+  return result;
+}
+
+
+int read_string(char *buf, int buf_siz)
+{
+  int counter = 0;
+  int a;
+  do
+    {
+      a = getchar();
+      buf[counter] = a;
+      counter++;
+      
+    }
+  while  (a !='\0' && a != '\n' && counter <= (buf_siz -1));
+
+  if (buf[counter-1] == '\n')
+    {
+      buf[counter-1] = '\0';
+    
+    }
+ 
+  return (counter-1);
+}
+
+
+char *ask_question_string(char *question, char *buf, int buf_siz)
+{
+  int chars=0;
+  do
+    {
+     printf("%s\n", question);
+     chars= read_string(buf, buf_siz);       
+    }
+  while (chars < 1);
+  return strdup(buf) ;
+}
+
