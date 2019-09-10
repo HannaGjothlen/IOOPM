a8af98973d64109653fc62a03663909e6e848c9e
SandraAkerman <47557395+SandraAkerman@users.noreply.github.com>
2019-09-10 10:49:57 +0200

    Add files via upload

--- swap.c
+++ swap.c
@@ -3,18 +3,16 @@
 void swap(int *a, int *b)
 {
   int tmp = *a;
-  a = b;
-  b = tmp;
+ *a = *b;
+ *b = tmp;
 }
 
 int main(void)
 {
 
-  a = &x;
-  b = &y; 
   int x = 7;
   int y = 42;
-  swap(x, y);
+  swap(&x, &y);
   printf("%d, %d\n", x, y);
   return 0;
 }
