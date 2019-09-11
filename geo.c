2923c5034fe959e25ef8dba76b901c69766f3663
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2019-09-11 15:41:27 +0200

    Add files via upload

--- geo.c
+++ geo.c
@@ -30,7 +30,7 @@ void translate(point_t *p1, point_t *p2)
 
 void print_point (point_t *p)
 {
-  printf("point(%d,%d)\n", p->x, p->y);
+  printf("point(%d,%d)", p->x, p->y);
 }
 
 
@@ -40,19 +40,51 @@ point_t make_point (int a, int b)
  return p;
 }
 
-void print_rect (rectangle_t *p)
-  
+void print_rect (rectangle_t *rec)
+
 {
-  print_point(&p);
-  printf("rectangle(upper_left=(%d), lower_right=(%d)\n"), p->x, p->y);
-}
+
+  point_t upp=(rec->upper_left);
+  point_t low=(rec->lower_right);
+
+  printf("rectangle(upper_left=");
+  print_point(&upp);
+  printf(", lower_right=");
+  print_point(&low);
+  printf(")\n");
   
+}
+
+rectangle_t  make_rect (int a, int b, int c, int d)
+
+{
+  point_t upper = make_point(a,b);
+  point_t lower =make_point(c,d);
+  rectangle_t r= {.upper_left=upper, .lower_right=lower};
+  return r;
+} 
+
+int area_rect(rectangle_t *r)
+{
+  point_t upp=(r->upper_left);
+  point_t low=(r->lower_right);
+  int a = (upp.y);
+  int b = (low.x);
+  int area = a * b;
+  return area;
+
+}
+
 int main()
  
 {
-  int c=1;
-  int d=2;
-  point_t p;
-  p = make_point(c,d);
-  print_point(&p);
+  int a=1;
+  int b=2;
+  int c=3;
+  int d=4;
+
+  rectangle_t r=make_rect(a,b,c,d);
+  print_rect(&r);
+   
 }
+
